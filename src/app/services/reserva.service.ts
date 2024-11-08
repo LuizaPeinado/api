9454580d9ae3eba3439863ac46603c6ce0dd059c
LuizaPeinado <luizapeinado@gmail.com>
2024-11-08 19:00:39 -0300

    retirando porta da url

--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -11,9 +11,9 @@ export class ReservaHotel {
   constructor(private http:HttpClient) { }
 
   getAllReserva(){
-    return this.http.get<any>("https://339b-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app:3000/reservas")
+    return this.http.get<any>("https://339b-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/reservas")
   }
   createReserva(reserva:Reserva){
-    return this.http.post(`https://339b-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app:3000/reservas`,reserva)
+    return this.http.post(`https://339b-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/reservas`,reserva)
   }
 }
