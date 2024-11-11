e42b7da370bab2d155b2b61580fed9d937b39f69
LuizaPeinado <luizapeinado@gmail.com>
2024-11-10 21:19:52 -0300

    add headers in hotel and reservas

--- src/app/services/hotel-service.ts
+++ src/app/services/hotel-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hotel } from '../interfaces/hotel';
 
@@ -6,13 +6,18 @@ import { Hotel } from '../interfaces/hotel';
   providedIn: 'root'
 })
 export class HotelService {
+  private url = 'https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/hotel'
+
 
   constructor(private http:HttpClient) { }
 
   getAllHotel(){
-    return this.http.get<any>("https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/hotel")
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning': 'true'  // Define o cabeçalho para evitar o aviso do ngrok
+    });
+    return this.http.get<any>(this.url,{headers})
   }
   createUser(hotel:Hotel){
-    return this.http.post(`https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/hotel`,hotel)
+    return this.http.post(this.url,hotel)
   }
 }
--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hotel } from '../interfaces/hotel';
 import { Reserva } from '../interfaces/reserva';
@@ -8,12 +8,17 @@ import { Reserva } from '../interfaces/reserva';
 })
 export class ReservaHotel {
 
+  private url = 'https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/reservas'
+
   constructor(private http:HttpClient) { }
 
   getAllReserva(){
-    return this.http.get<any>("https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/reservas")
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning': 'true'  // Define o cabeçalho para evitar o aviso do ngrok
+    });
+    return this.http.get<any>(this.url, {headers})
   }
   createReserva(reserva:Reserva){
-    return this.http.post(`https://d924-2804-7f0-9281-aba4-1d2c-cbf3-291c-e171.ngrok-free.app/reservas`,reserva)
+    return this.http.post(this.url,reserva)
   }
 }
