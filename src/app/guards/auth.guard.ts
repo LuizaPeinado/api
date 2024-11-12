5727a649dfba06e50f250f527296bf116ace4c96
LuizaPeinado <luizapeinado@gmail.com>
2024-11-12 13:53:33 -0300

    FIX: localstora bug

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,23 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const router= inject(Router)
+  const platformId = inject(PLATFORM_ID);
 
-  const localData = localStorage.getItem('angularToken')
-  if(localData != null){
-    return true;
-  }else{
-    router.navigateByUrl("/login")
+  if (isPlatformBrowser(platformId)) {
+    const localData = localStorage.getItem('angularToken');
+    if (localData != null) {
+      return true;
+    } else {
+      router.navigateByUrl("/login");
+      return false;
+    }
+  } else {
+    // Se não está no navegador, impede a navegação
+    router.navigateByUrl("/login");
     return false;
-
   }
 };
